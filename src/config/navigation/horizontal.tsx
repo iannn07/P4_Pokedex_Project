cee6a0bc1ea8bb401c0fb531a9f3eabb7508e916
Iannn <pristianb007@gmail.com>
2024-03-13 19:13:19 +0700

    Side Bar Navigation Efficiency

--- src/config/navigation/horizontal.tsx
+++ src/config/navigation/horizontal.tsx
@@ -1,32 +1,28 @@
 import {
-  File,
-  Lock,
-  Mail,
-  Shield,
-  SmartHome,
   Backpack,
   Pokeball,
-} from "@nxweb/icons/tabler";
+  SmartHome
+} from '@nxweb/icons/tabler';
 
 import type { HorizontalNavItemsType } from '@layouts/types.js';
 
 export const navigation: readonly HorizontalNavItemsType[] = [
   {
     icon: <SmartHome />,
-    id: "home",
-    link: "/home",
-    text: "Home",
+    id: 'home',
+    link: '/home',
+    text: 'Home'
   },
   {
     icon: <Backpack />,
-    id: "inventory",
-    link: "/inventory",
-    text: "Inventory",
+    id: 'inventory',
+    link: '/inventory',
+    text: 'Inventory'
   },
   {
     icon: <Pokeball />,
-    id: "pokemon-list",
-    link: "/pokemonlist",
-    text: "Pokemon List",
-  },
+    id: 'pokemon-list',
+    link: '/pokemonlist',
+    text: 'Pokemon List'
+  }
 ];
