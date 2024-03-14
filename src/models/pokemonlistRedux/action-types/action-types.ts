8ec6955ba94b92b4b78b58e5746693e275b831c7
Iannn <pristianb007@gmail.com>
2024-03-15 00:34:25 +0700

    Redux - Sleep

--- src/models/pokemonlistRedux/action-types/action-types.ts
+++
@@ -1,4 +0,0 @@
-export enum ActionTypes {
-  OPEN_DIALOG = 'OPEN_DIALOG',
-  CLOSE_DIALOG = 'CLOSE_DIALOG'
-}
--- src/models/pokemonlistRedux/store.ts
+++
@@ -1,7 +0,0 @@
-import { createStore } from '@nxweb/core';
-
-import dialogReducer from './reducer/dialogReducer';
-
-const store = createStore(dialogReducer, { open: false });
-
-export default store;
