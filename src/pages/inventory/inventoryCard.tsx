17bc9c466407ccf1485f068cebd3e23635f05773
Daffeyd Wilbert <daffeydwilbert@gmail.com>
2024-03-20 11:39:55 +0700

    Alert in invertory page

--- src/pages/inventory/inventoryCard.tsx
+++ src/pages/inventory/inventoryCard.tsx
@@ -120,181 +120,189 @@ const InventoryCard: React.FC<InventoryPokemonsModel> = ({ inventory }) => {
   };
 
   return (
-    <>
     <Slide direction="up" in={true} mountOnEnter={true} unmountOnExit={true}>
-        <Card
+      <Card
+        sx={{
+          overflowX: 'auto',
+          padding: '10px 40px 25px 40px'
+        }}
+      >
+        <Box
           sx={{
-            overflowX: 'auto',
-            p: [5, 10]
+            display: 'grid',
+            gridTemplateColumns: '300px 1fr',
+            mb: 1,
+            height: '50px',
+            alignItems: 'center',
+            justifyContent: 'space-between'
           }}
         >
-          <Box
-            sx={{
-              mb: 8
-            }}
-          >
-            <Typography sx={{ fontWeight: 'bold' }} variant="subtitle1">Kidnapped Pokemon <Badges /><Pokeball /></Typography>
-          </Box>
-          <Grid
-            container={true}
-            spacing={6}
-            sx={{
-              alignItems: 'flex-start',
-              display: 'flex',
-              flexWrap: 'nowrap'
-            }}
-          >
-            {inventory?.map((data, index) => (
-              <Grid item={true} key={index}>
-                <Box>
-                  <Card sx={{ p: 3 }}>
-                    <CardHeader
-                      action={<Trash
+          <Typography sx={{ fontWeight: 'bold' }} variant="subtitle1">
+            Kidnapped Pokemon <Badges />
+            <Pokeball />
+          </Typography>
+          {errorMessage ? <Alert severity="error" sx={{ flexBasis: '100%' }}>{errorMessage}</Alert> : null}
+          {successMessage
+            ? <Alert severity="success">{successMessage}</Alert>
+            : null}
+        </Box>
+        <Grid
+          container={true}
+          spacing={6}
+          sx={{
+            alignItems: 'flex-start',
+            display: 'flex',
+            flexWrap: 'nowrap'
+          }}
+        >
+          {inventory?.map((data, index) => (
+            <Grid item={true} key={index}>
+              <Box>
+                <Card sx={{ p: 3 }}>
+                  <CardHeader
+                    action={
+                      <Trash
                         cursor="pointer"
                         height="25px"
                         width="25px"
-                        onClick={() => handleRemove(data)} />} />
+                        onClick={() => handleRemove(data)} />
+                    } />
 
-                    <Grid container={true} sx={{ flexWrap: 'nowrap' }}>
-                      <Grid item={true} sx={{ width: '16rem' }}>
-                        <CardMedia
-                          component="img"
-                          image={data.image_url}
-                          sx={{
-                            height: '10rem',
-                            objectFit: 'contain'
-                          }} />
+                  <Grid container={true} sx={{ flexWrap: 'nowrap' }}>
+                    <Grid item={true} sx={{ width: '16rem' }}>
+                      <CardMedia
+                        component="img"
+                        image={data.image_url}
+                        sx={{
+                          height: '10rem',
+                          objectFit: 'contain'
+                        }} />
 
-                        <CardContent
+                      <CardContent
+                        sx={{
+                          alignItems: 'center',
+                          justifyContent: 'center'
+                        }}
+                      >
+                        <Typography
+                          sx={{ fontWeight: 'bold', mb: 3 }}
+                          variant="h4"
+                        >
+                          {data.pokemon}
+                        </Typography>
+
+                        <Box
                           sx={{
-                            alignItems: 'center',
-                            justifyContent: 'center'
+                            borderRadius: 8,
+                            display: 'flex',
+                            gap: 2,
+                            height: 'auto',
+                            mb: 2
                           }}
                         >
-                          <Typography
-                            sx={{ fontWeight: 'bold', mb: 3 }}
-                            variant="h4"
-                          >
-                            {data.pokemon}
-                          </Typography>
-
-                          <Box
-                            sx={{
-                              borderRadius: 8,
-                              display: 'flex',
-                              gap: 2,
-                              height: 'auto',
-                              mb: 2
-                            }}
-                          >
-                            {data.type.split('/').map((type) => (
-                              <Box
-                                key={type}
-                                px={4}
-                                py={1.5}
+                          {data.type.split('/').map((type) => (
+                            <Box
+                              key={type}
+                              px={4}
+                              py={1.5}
+                              sx={{
+                                backgroundColor: getColorForType(type),
+                                borderRadius: 8,
+                                display: 'flex',
+                                gap: 8
+                              }}
+                            >
+                              <Typography
                                 sx={{
-                                  backgroundColor: getColorForType(type),
-                                  borderRadius: 8,
-                                  display: 'flex',
-                                  gap: 8
+                                  color: 'white',
+                                  fontSize: 10,
+                                  letterSpacing: 1.5
                                 }}
                               >
-                                <Typography
-                                  sx={{
-                                    color: 'white',
-                                    fontSize: 10,
-                                    letterSpacing: 1.5
-                                  }}
-                                >
-                                  {type.trim()}
-                                </Typography>
-                              </Box>
-                            ))}
-                          </Box>
-                          <Typography variant="h6">Obtained At</Typography>
-                          <Typography sx={{ fontWeight: 'bold' }} variant="h6">
-                            {data.date}
-                          </Typography>
+                                {type.trim()}
+                              </Typography>
+                            </Box>
+                          ))}
+                        </Box>
+                        <Typography variant="h6">Obtained At</Typography>
+                        <Typography sx={{ fontWeight: 'bold' }} variant="h6">
+                          {data.date}
+                        </Typography>
 
-                          {!data?.evolutions
-                            ?.map((evolution) => {
+                        {!data?.evolutions
+                          ?.map((evolution) => {
+                            const evolutionExists: Pokemons | undefined =
+                              state?.pokemons?.pokemons?.find(
+                                (pokemon) => evolution === pokemon.pokemon
+                              );
+                            // eslint-disable-next-line no-eq-null
+                            if (evolutionExists != null) return true;
+
+                            return false;
+                          })
+                          .some(Boolean) ? null : (
+                          <Button
+                            disabled={false}
+                            key={index}
+                            sx={{ mt: 6 }}
+                            variant="contained"
+                            onClick={() => handleChange(index)}
+                          >
+                            Check Evolution <ArrowRight />
+                          </Button>
+                          )}
+                      </CardContent>
+                    </Grid>
+                    <Collapse
+                      in={checked[index]}
+                      key={index}
+                      orientation="horizontal"
+                    >
+                      <Grid item={true} sx={{ width: '8rem' }}>
+                        <Typography sx={{ mb: 0 }} variant="h5">
+                          Available Evolution :
+                        </Typography>
+                        {data?.evolutions?.length > 0 && (
+                          <List>
+                            {data?.evolutions?.map((evolution, indexEvo) => {
                               const evolutionExists: Pokemons | undefined =
                                 state?.pokemons?.pokemons?.find(
-                                  (pokemon) => evolution === pokemon.pokemon
+                                  (pokemon) => pokemon.pokemon === evolution
                                 );
-                              // eslint-disable-next-line no-eq-null
-                              if (evolutionExists != null) return true;
 
-                              return false;
-                            })
-                            .some(Boolean) ? null : (
-                              <Button
-                                disabled={false}
-                                key={index}
-                                sx={{ mt: 6 }}
-                                variant="contained"
-                                onClick={() => handleChange(index)}
-                              >
-                                Check Evolution <ArrowRight />
-                              </Button>
-                            )}
-                        </CardContent>
+                              const isDisabled = !evolutionExists;
+                              if (!evolutionExists) return null;
+
+                              return (
+                                <Button
+                                  disabled={isDisabled}
+                                  key={indexEvo}
+                                  sx={{ mt: 6 }}
+                                  variant="contained"
+                                  {...(evolutionExists && {
+                                    onClick: () => handleEvolve(
+                                      data,
+                                      evolutionExists,
+                                      index
+                                    )
+                                  })}
+                                >
+                                  {evolution}
+                                </Button>
+                              );
+                            })}
+                          </List>
+                        )}
                       </Grid>
-                      <Collapse
-                        in={checked[index]}
-                        key={index}
-                        orientation="horizontal"
-                      >
-                        <Grid item={true} sx={{ width: '8rem' }}>
-                          <Typography sx={{ mb: 0 }} variant="h5">
-                            Available Evolution :
-                          </Typography>
-                          {data?.evolutions?.length > 0 && (
-                            <List>
-                              {data?.evolutions?.map((evolution, indexEvo) => {
-                                const evolutionExists: Pokemons | undefined =
-                                  state?.pokemons?.pokemons?.find(
-                                    (pokemon) => pokemon.pokemon === evolution
-                                  );
-
-                                const isDisabled = !evolutionExists;
-                                if (!evolutionExists) return null;
-
-                                return (
-                                  <Button
-                                    disabled={isDisabled}
-                                    key={indexEvo}
-                                    sx={{ mt: 6 }}
-                                    variant="contained"
-                                    {...(evolutionExists && {
-                                      onClick: () => handleEvolve(
-                                        data,
-                                        evolutionExists,
-                                        index
-                                      )
-                                    })}
-                                  >
-                                    {evolution}
-                                  </Button>
-                                );
-                              })}
-                            </List>
-                          )}
-                        </Grid>
-                      </Collapse>
-                    </Grid>
-                  </Card>
-                </Box>
-              </Grid>
-            ))}
-          </Grid>
-        </Card>
+                    </Collapse>
+                  </Grid>
+                </Card>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Card>
     </Slide>
-      {errorMessage ? <Alert severity="error">{errorMessage}</Alert> : null}
-      {successMessage
-        ? <Alert severity="success">{successMessage}</Alert>
-        : null}
-    </>
   );
 };
 
